640513faec65de0864cee4e0cef8717937cebb07
Nico Wehmöller <nico.wehmoeller@eripini.win>
2018-01-05 10:05:20 +0100

    added sponsor related methods

--- api.js
+++ api.js
@@ -3,11 +3,20 @@ let filter = require('array-filter');
 
 const API_HOST = "https://api.utopian.io/api";
 const ENDPOINT_MODERATORS = API_HOST + '/moderators';
+const ENDPOINT_SPONSORS = API_HOST + '/sponsors';
 
 let utopian = {};
 utopian.getModerators = () => {
     return new Promise((yes, no) => {
         request(ENDPOINT_MODERATORS, [], (err, response, body) => {
+            if (err) no(err);
+            yes(body);
+        })
+    });
+};
+utopian.getSponsors = () => {
+    return new Promise((yes, no) => {
+        request(ENDPOINT_SPONSORS, [], (err, response, body) => {
             if (err) no(err);
             yes(body);
         })
@@ -26,6 +35,18 @@ utopian.isModerator = (username) => {
         });
     })
 };
-
+utopian.isSponsor = (username) => {
+    return new Promise((yes, no) => {
+        utopian.getSponsors().then((sponsors) => {
+            yes(filter(JSON.parse(sponsors).results, (sponsor) => {
+                if (sponsor.account === username) {
+                    return sponsor
+                } else {
+                    return false;
+                }
+            }))
+        });
+    })
+};
 
 module.exports = utopian;
